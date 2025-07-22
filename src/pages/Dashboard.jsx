dde1a35f4d74c1abfb3c1b6a93d319cecb47c87c
tlc3004 <leonantoniobm@gmail.com>
2025-07-22 15:17:54 -0500

    se me paso

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import ProductButton from '../components/ProductButton'
 import Receipt from '../components/Receipt'
 
-export default function Dashboard({ productos, onAgregar, setProductos, onClosed }) {
+export default function Dashboard({ productos, onAgregar, setProductos }) {
   const [seleccionados, setSeleccionados] = useState([])
   const [mostrarBoleta, setMostrarBoleta] = useState(false)
   const [categoriaSeleccionada, setCategoriaSeleccionada] = useState('todas')
