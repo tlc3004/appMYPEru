b552dbdd8636a041cb7fec11b0ede380c77e344b
tlc3004 <leonantoniobm@gmail.com>
2025-07-26 03:17:07 -0500

    siempre yo olvidandome

--- src/components/GuideUse.jsx
+++ src/components/GuideUse.jsx
@@ -4,7 +4,7 @@ import '../styles/styles.css'
 const pasos = [
   {
     titulo: '1. Añadir Productos',
-    descripcion: 'Usa el formulario para ingresar productos(ó usar los predeterminados) con nombre, precio y categoría.',
+    descripcion: 'Usa el formulario para ingresar productos por categoria , su nombre y el precio.(algunas categorias contiene icono)',
     emoji: '📝'
   },
   {
