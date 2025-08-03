22f7f93d7e122488cf0ac1478bfa5f33b7ff27ce
tlc3004 <leonantoniobm@gmail.com>
2025-08-03 16:28:02 -0500

    yayayay

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.html"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'mype-rojo': '#B91C1C',
+        'mype-verde': '#16A34A',
+        'mype-azul': '#1E3A8A',
+      },
+    },
   },
   plugins: [],
-};
+}
